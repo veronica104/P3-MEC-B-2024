54fadb0a3a24fd812e6d33a6b6c6bd02511e45b2
Veronica Duarte <isabelduarte413@gmail.com>
2024-08-27 14:51:08 -0500

    pg2.java
    punto 2 del parcial

---
+++ P1/pg2.java
@@ -0,0 +1,22 @@
+//con la misma clase de  la pregunta anterior,crear una sobrecarga del contructor y crear un objeto con cado una de sus formas//
+package prueba;
+
+public class fraccion  {
+
+    public fraccion(int par, int par1, int par2) {
+    }
+    int z;
+    int x;
+    int y;
+    
+    public static void main(String[] args) {
+        
+     fraccion frac =new fraccion(2, 4, 7);
+     
+     int x =frac.x;
+     int y =frac.y;
+     int z =frac.z;
+    
+    }
+    
+}
