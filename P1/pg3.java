e65404b8562a8efc5d41d69dc8112ce020da89b1
Veronica Duarte <isabelduarte413@gmail.com>
2024-08-27 15:01:58 -0500

    pg3.java
    punto 3 del parcial

---
+++ P1/pg3.java
@@ -0,0 +1,21 @@
+//con la misma clase de  la pregunta anterior, crear un metodo puro que me permita sumar dos fraciones y me devuelva su resultado//
+package prueba;
+
+public class fraccion  {
+    
+   int d1=3;
+   int d2=5;
+   int n1=4;
+   int n2=6;
+   int resultado;
+   
+    public static void main(String[] args) {
+        
+     public metodo(){
+       resultado = (d1/n1)+(d2/n2);
+       
+       return resultado;
+      }
+    }
+     
+}
