58e306e98cd40a91fdcfd0bd25889321f01564cb
Veronica Duarte <isabelduarte413@gmail.com>
2024-08-27 14:35:25 -0500

    pg1.java
    primer punto parcial

---
+++ P1/pg1.java
@@ -0,0 +1,24 @@
+//crear una clase llamada fraccion , con atibutos correspondientes a denominador , numerador ;manejar encapsulamiento y los metodos get y set
+package prueba;
+
+public class fraccion {
+    
+    private int denominador=0;
+    private int numerador=0;
+    
+    public static void main(String[] args) {
+        
+        int deno=4;
+        int num=3
+        
+      this.denominador =deno;
+      this.numerador=num;
+        
+       fraccion frac = new fraccion();
+       
+       frac.get(numerodor);
+    
+        
+    }
+    
+}
